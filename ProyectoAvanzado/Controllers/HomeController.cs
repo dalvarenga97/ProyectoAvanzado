a04011422afbb4c037003218b36943f21fab2a98
dalvarenga <dalvarenga@DALVARENGA-Z>
2019-10-10 12:43:11 -0300

    Proyecto con mejoras!

--- ProyectoAvanzado/Controllers/HomeController.cs
+++ ProyectoAvanzado/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace ProyectoAvanzado.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Nuestra Política de Calidad está orientada a la satisfacción de mejorar continuamente nuestros servicios de salud, estimular su desarrollo integral a través de la capacitación permanente y los beneficios personales, mantener la rentabilidad y el posicionamiento de la empresa, constituyendo una opción segura, honesta y eficiente.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Calle Eligio Ayala 1383 y Paí Perez// Asunción - Paraguay // Central Telefónica (021) 248 9000.";
 
             return View();
         }
