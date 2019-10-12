08a3dfa6987d4da26b819799f1321b1a0b250ce8
dalvarenga <dalvarenga@DALVARENGA>
2019-10-12 18:11:16 -0300

    modificado contacto

--- ProyectoAvanzado/Controllers/ContactoController.cs
+++ ProyectoAvanzado/Controllers/ContactoController.cs
@@ -13,7 +13,27 @@ namespace ProyectoAvanzado.Controllers
     public class ContactoController : Controller
     {
         private MediCsharp2Entities db = new MediCsharp2Entities();
-        
+
+        // GET: Contacto
+        public ActionResult Index()
+        {
+            return View(db.Contacto.ToList());
+        }
+
+        // GET: Contacto/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contacto contacto = db.Contacto.Find(id);
+            if (contacto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contacto);
+        }
 
         // GET: Contacto/Create
         public ActionResult Create()
@@ -38,8 +58,63 @@ namespace ProyectoAvanzado.Controllers
             return View(contacto);
         }
 
+        // GET: Contacto/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contacto contacto = db.Contacto.Find(id);
+            if (contacto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contacto);
+        }
 
-       
+        // POST: Contacto/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Email,Password,Comentario")] Contacto contacto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contacto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(contacto);
+        }
+
+        // GET: Contacto/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contacto contacto = db.Contacto.Find(id);
+            if (contacto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contacto);
+        }
+
+        // POST: Contacto/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Contacto contacto = db.Contacto.Find(id);
+            db.Contacto.Remove(contacto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
--- ProyectoAvanzado/Models/Contacto.cs
+++ ProyectoAvanzado/Models/Contacto.cs
@@ -16,7 +16,7 @@ namespace ProyectoAvanzado.Models
     {
         public int Id { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
+        public string Nombre { get; set; }
         public string Comentario { get; set; }
     }
 }
