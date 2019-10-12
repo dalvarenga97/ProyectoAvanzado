c1d66a568ab74cb2a9db49bbec2f945045b5d09d
gerardlezcan <gerardlezcano@gmail.com>
2019-10-12 17:59:31 -0400

    Crear un formulario que guarde a la base de datos las consultas realizadas

---
+++ ProyectoAvanzado/Controllers/ContactoController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoAvanzado.Models;
+
+namespace ProyectoAvanzado.Controllers
+{
+    public class ContactoController : Controller
+    {
+        private MediCsharp2Entities db = new MediCsharp2Entities();
+        
+
+        // GET: Contacto/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Contacto/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Email,Password,Comentario")] Contacto contacto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Contacto.Add(contacto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(contacto);
+        }
+
+
+       
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
