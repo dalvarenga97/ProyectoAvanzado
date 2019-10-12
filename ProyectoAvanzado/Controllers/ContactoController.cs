3ce1d15a0aeabdd6ee295a2a871f6acd4832297d
gerardlezcan <gerardlezcano@gmail.com>
2019-10-12 18:30:08 -0400

    contacto listo

--- ProyectoAvanzado/Controllers/ContactoController.cs
+++ ProyectoAvanzado/Controllers/ContactoController.cs
@@ -20,20 +20,7 @@ namespace ProyectoAvanzado.Controllers
             return View(db.Contacto.ToList());
         }
 
-        // GET: Contacto/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Contacto contacto = db.Contacto.Find(id);
-            if (contacto == null)
-            {
-                return HttpNotFound();
-            }
-            return View(contacto);
-        }
+        
 
         // GET: Contacto/Create
         public ActionResult Create()
@@ -46,7 +33,7 @@ namespace ProyectoAvanzado.Controllers
         // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Email,Password,Comentario")] Contacto contacto)
+        public ActionResult Create([Bind(Include = "Id,Email,Nombre,Comentario")] Contacto contacto)
         {
             if (ModelState.IsValid)
             {
@@ -58,62 +45,7 @@ namespace ProyectoAvanzado.Controllers
             return View(contacto);
         }
 
-        // GET: Contacto/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Contacto contacto = db.Contacto.Find(id);
-            if (contacto == null)
-            {
-                return HttpNotFound();
-            }
-            return View(contacto);
-        }
-
-        // POST: Contacto/Edit/5
-        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
-        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Email,Password,Comentario")] Contacto contacto)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(contacto).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(contacto);
-        }
-
-        // GET: Contacto/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Contacto contacto = db.Contacto.Find(id);
-            if (contacto == null)
-            {
-                return HttpNotFound();
-            }
-            return View(contacto);
-        }
-
-        // POST: Contacto/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Contacto contacto = db.Contacto.Find(id);
-            db.Contacto.Remove(contacto);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+       
 
         protected override void Dispose(bool disposing)
         {
